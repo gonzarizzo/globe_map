e4daf47c01922799e8732aeeec5bdf0d601cc009
Gonzalo Rizzo <gonrizzo@gmail.com>
2024-08-25 20:03:46 -0300

    Created a bash script to download map of the world.

--- workflow/scripts/get_global_map.bash
+++ workflow/scripts/get_global_map.bash
@@ -1,3 +1,11 @@
-# shebang
+#!/usr/bin/env bash
 
-wget -0 ../resources/ "https://giscollective.s3.amazonaws.com/projectlinework/moriarty-hand.zip"
+url="https://giscollective.s3.amazonaws.com/projectlinework/moriarty-hand.zip"
+
+# make a directory to store data
+mkdir resources/
+
+# download shapefiles, unzip shape and remove zip file
+wget -O resources/moriarty-hand.zip $url \
+&& unzip -o resources/moriarty-hand.zip -d resources/ \
+&& rm resources/moriarty-hand.zip
